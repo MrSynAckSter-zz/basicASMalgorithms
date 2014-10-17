2ada97a3dee39c21d9737079532d383af562d6b9
John Dunlap <freejazztampa@yahoo.com>
2014-10-17 19:13:19 -0400

    More RAX things, still not a working patch. WAHOO

--- fizzbuzz.asm
+++ fizzbuzz.asm
@@ -6,20 +6,35 @@ section .text
 
 
 _start:
-	add edx, 1 
-	cmp edx,0x64
-    pushq rdx 
+	add edx, 1 ; Add one 
+	cmp edx,0x64 ; if it's 100, set a flag. This will have to move lower
+    pushq rdx    ; Save the value 
    
- 
+    mov eax, edx 
+    mov ebx, 7 
+    div ebx
+    
+    pushq rax
+    
+    mov eax, edx
+    mov ebx, 9 
+    div ebx, 
+    cmp eax, 0
+
+    pushq rax
+    
+
+    popq rax ; Remainder from 9 operation  
+    popq rbx ; Remainder from 7 operation 
+
+     
 	mov edx,len
 	mov ecx,msg
 	mov ebx, 1
 	mov eax, 4
 	int 0x80 
-	;Modulo 	
     popq rdx 
 	jnz _start
-	;Add line feeds? 
 	; Convert dword to Ascii 
 	mov	eax, 1 ; sys_exit
 	int	0x80   ; call kernal 
