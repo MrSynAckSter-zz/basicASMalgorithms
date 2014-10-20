1a7eb8b4a9f1ac87564834152f9e66d09c4f96be
John <johndunlapdev@gmail.com>
2014-10-19 21:56:37 -0400

    fixed some formatting bugs. Still not done with even one of these.

--- fizzbuzz.asm
+++ fizzbuzz.asm
@@ -6,32 +6,30 @@ section .text
 
 
 _start:
-	add edx, 1 ; Add one 
-	cmp edx,0x64 ; if it's 100, set a flag. This will have to move lower
-    pushq   rdx    ; Save the value 
+	add     edx, 1   ; Add one. Implicitly we start at 0  
+	cmp     edx,0x64 ; if it's 100, set a flag. This will have to move lower
+	pushq   rdx      ; Save the value 
    
-    mov eax, edx   
-    mov ebx, 7 
-    div ebx
+        mov     eax, edx ; Move the value to edx so it may be devided (devided clobbers eax)   
+        mov     ebx, 7   ; Move the dvisor 
+        div     ebx      ; divide 
     
-    pushq rax ; Hold on to remainder
+        pushq   rax      ; Hold on to remainder
     
-    mov eax, edx
-    mov ebx, 9 
-    div ebx, 
-    cmp eax, 0
+        mov     eax, edx ; Move that same value over into  
+        mov     ebx, 9   ; Prepare to divide by 9
+        div     ebx      ; divide 
 
-    pushq rax ; Hold on to Remainder 
+        pushq   rax      ; Hold on to Remainder 
     
+        popq    rax      ; Remainder from 9 operation  
+        popq    rbx      ; Remainder from 7 operation 
 
-    popq rax ; Remainder from 9 operation  
-    popq rbx ; Remainder from 7 operation 
-
-    comp rax rbx; 
+        comp    rax, rbx ; this wont work 
                 
-    jnz  _fizz
-    jk   _fizzbuzz
-    jz   _buzz 
+        jnz  _fizz
+        jk   _fizzbuzz
+        jz   _buzz 
 
      
 	mov edx,len
@@ -39,11 +37,11 @@ _start:
 	mov ebx, 1
 	mov eax, 4
 	int 0x80 
-    popq rdx 
+        popq rdx 
 	jnz _start
-	; Convert dword to Ascii 
-	mov	eax, 1 ; sys_exit
-	int	0x80   ; call kernal 
+	                 ; Convert dword to Ascii 
+	mov	eax, 1   ; sys_exit
+	int	0x80     ; call kernal 
 
 section .data 
 	msg db 'FizzBuzz', 0xa 
