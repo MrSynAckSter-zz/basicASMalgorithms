402e0affcadfeec74b1ec24ff62c1a81484bd269
John Dunlap <freejazztampa@yahoo.com>
2014-10-23 23:51:33 -0400

    Added more, minor changes

---
+++ longestcommonsequence.asm
@@ -0,0 +1,28 @@
+;Merge Sort 
+global _start 
+
+section .text 
+
+_start:
+
+; Loosely based on http://rosettacode.org/wiki/Sorting_algorithms/Merge_sort#C
+    startArray db 0x32, 0x44, 0x55, 0x78, 0x81, 0x92, 0x88, 0x11, 0x01, 0x02
+    ; Maybe this goes in the data section? 
+    startlen equ $-startArray ; or something look this up. 
+    ;as a policy we just write down the length of each array as we go.     
+     
+_merge: 
+;push and pop etc...
+;Requires a list, left start, left end , right start, right end
+
+
+_mergesort:
+
+_mergesort_r: 
+
+_print_list: 
+
+
+ 
+
+    
--- mergesort.asm
+++ mergesort.asm
@@ -1,28 +1,39 @@
-;Merge Sort 
+;Merge Sort
+ 
+ ;Loosely based on http://rosettacode.org/wiki/Sorting_algorithms/Merge_sort#C
 global _start 
 
 section .text 
 
 _start:
+    
+; Print Values before hand 
 
-; Loosely based on http://rosettacode.org/wiki/Sorting_algorithms/Merge_sort#C
-    startArray db 0x32, 0x44, 0x55, 0x78, 0x81, 0x92, 0x88, 0x11, 0x01, 0x02
-    ; Maybe this goes in the data section? 
-    startlen equ $-startArray ; or something look this up. 
-    ;as a policy we just write down the length of each array as we go.     
+_mergesort:
+    pushq rcx
+    pushq rbx 
+    pushq rax
+
+_mergesort_r:
+     
+   ;Return value goes in EAX  
+   
      
 _merge: 
 ;push and pop etc...
 ;Requires a list, left start, left end , right start, right end
 
 
-_mergesort:
 
-_mergesort_r: 
 
 _print_list: 
 
 
  
 
+section .data 
+    startArray db 0x32, 0x44, 0x55, 0x78, 0x81, 0x92, 0x88, 0x11, 0x01, 0x02
+    ; Maybe this goes in the data section? 
+    startlen equ $-startArray ; or something look this up. 
+    ;as a policy we just write down the length of each array as we go.     
     
