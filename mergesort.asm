913ef903cb3f1220f4a357f831534806a87930bf
John Dunlap <freejazztampa@yahoo.com>
2014-10-24 00:32:10 -0400

    More updates

--- mergesort.asm
+++ mergesort.asm
@@ -10,14 +10,42 @@ _start:
 ; Print Values before hand 
 
 _mergesort:
-    pushq rcx
-    pushq rbx 
-    pushq rax
-
+    pushq startArray ; Pointer to the list 
+    pushq startlen ; Length of the list. Should be predetermined
+    pushq rax ; "Left" intialized to 0 the first go round 
+    
+    ; Return control or exit or something 
 _mergesort_r:
+    ; Pop or something
+    popq rax ; left
+    popq rbx ; Right 
+    popq rcx ; Pointer to list
+    ; Replace the pops with stack manipulation 
+    
+    mov rdx, rax ; Move left into a spot where we can keep the result
+    sub rdx, rbx
+    cmp rdx, 0x01 ; if right - left  is less than or equal 1
+    
+    jle returnProc ; return if that's the case 
      
-   ;Return value goes in EAX  
+   ;Return value goes in EAX 
+    
+   ;set up stack for recurssion 
    
+   pushq THECORRECTCURRENTLOCATIONOFTHELIST ; The current list 
+    
+   mov rdx, rax ; move left to rdx for computation 
+   add rdx, rax ; left + right 
+   ; rivide rdx by two to get left end  
+   pushq rax ; left_start
+
+    jmp merge ; Most likely merge should be inlined 
+
+    ;return control 
+   
+_returnProc:
+;perhaps do some stack cleaning here. 
+ret      
      
 _merge: 
 ;push and pop etc...
@@ -27,9 +55,6 @@ _merge:
 
 
 _print_list: 
-
-
- 
 
 section .data 
     startArray db 0x32, 0x44, 0x55, 0x78, 0x81, 0x92, 0x88, 0x11, 0x01, 0x02
