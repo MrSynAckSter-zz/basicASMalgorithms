7690f1a61cfb2fa87d51f41499b8ff15422e7e78
John <johndunlapdev@gmail.com>
2014-10-26 02:51:56 -0400

    Fizzbuzz is done

--- fizzbuzz.asm
+++ fizzbuzz.asm
@@ -1,40 +1,41 @@
-;Fizz Buzz 
-
+;FizzBuzz
 global _start
 
 section .text
 
-
 _start:
-	add     eax, 1   ; Add one. Implicitly we start at 0  
-	pushq   rax      ; Save the value 
+	mov    rdx, 0   ; Zero out rdx. Else we get FPE
+	add    rax, 1   ; Add one. Implicitly we start at 0  
+	push   rax      ; Save the value 
    
-        mov     ebx, 7   ; Load  the dvisor 
-        div     ebx      ; divide 
+        mov    rbx, 7   ; Load  the dvisor 
+        div    rbx      ; divide 
     
-	popq    rax      ; Restore the current index 
-	pushq   rax      ; Hold onto the index 
-        pushq   rdx      ; Hold on to remainder
-	 
-        mov     ebx, 9   ; Prepare to divide by 9
-        div     ebx      ; divide 
-	
-        pushq   rdx      ; Hold on to Remainder 
-    
-        popq    rax      ; Remainder from 9 operation  
-        popq    rbx      ; Remainder from 7 operation 
+	pop    rax      ; Restore the current index 
+	push   rax      ; Hold onto the index 
+        push   rdx      ; Hold on to remainder
 
-        cmp    rbx, 0   ; if i % 7 == 0 
+	mov     rdx, 0  ; Zero out the rdx for clean divides 	 
+        mov     rbx, 9   ; Prepare to divide by 9
+        div     rbx      ; divide 
+	
+        push   rdx      ; Hold on to Remainder 
+    
+        pop    rax      ; Remainder from 9 operation  
+        pop    rbx      ; Remainder from 7 operation 
+
+        cmp     rbx, 0   ; if i % 7 == 0 
 	
         je      _true    ;Push a 1 for true to the stack 
         jne     _false   ;Push a 0 for false to the stack 
 
+_loopOrQuit: 
        
-        popq    rax      ; Pop our index.  
-	cmp     edx,0x64 ; if it's 100, set a flag. This will have to move lower
+        pop     rax      ; Pop our index.  
+	cmp     rax,0x64 ; if it's 100, set a flag. This will have to move lower
 	jne     _start
 	                 ; Convert dword to Ascii would be called here to print the index.  
-	mov	eax, 1   ; sys_exit
+	mov	rax, 1   ; sys_exit
 	int	0x80     ; call kernal 
 	
 _true:
@@ -43,65 +44,65 @@ _true:
 	cmp  rax, 0      ; is rax 0? 
 	je   _fizzBuzz   ; It is. FIZZBUZZ 
 	jne  _fizz 	 ; if it's not, then we are in a FIZZ only situation. 
-	ret
+	
 
 _false: 
 	cmp  rax, 0     ; We already know that rbx aint zero. The only thing left 
 	je   _buzz
-	ret 
+	jne  _loopOrQuit 
 
 
 _fizz: 
-	pushq rdx 
-	pushq rcx 
-	pushq rbx
-	pushq rax 
-	mov edx, fizzlen
-	mov ecx, fizzMsg 
-	mov ebx, 1
-	mov eax, 4
+	push rdx 
+	push rcx 
+	push rbx
+	push rax 
+	mov rdx, fizzlen
+	mov rcx, fizzMsg 
+	mov rbx, 1
+	mov rax, 4
 	int 0x80  
-	popq rax
-	popq rbx 
-	popq rcx
-	popq rdx 
-	ret
+	pop rax
+	pop rbx 
+	pop rcx
+	pop rdx 
+	jmp _loopOrQuit 
 _buzz:
-	pushq rdx
-	pushq rcx 
-	pushq rbx
-	pushq rax 
-	mov edx, buzzlen 
-	mov ecx, buzzMsg
-	mov ebx, 1
-	mov eax, 4
+	push rdx
+	push rcx 
+	push rbx
+	push rax 
+	mov rdx, buzzlen 
+	mov rcx, buzzMsg
+	mov rbx, 1
+	mov rax, 4
 	int 0x80 
-	popq rax
-	popq rbx
-	popq rcx
-	popq rdx 
-	ret
+	pop rax
+	pop rbx
+	pop rcx
+	pop rdx 
+	jmp  _loopOrQuit 
 
 _fizzBuzz:
-	pushq rdx 
-	pushq rcx
-	pushq rax 
-	pushq rbx
-	mov edx, len
-	mov ecx, msg
-	mov ebx, 1
-	mov eax, 4
+	push rdx 
+	push rcx
+	push rax 
+	push rbx
+	mov rdx, len
+	mov rcx, msg
+	mov rbx, 1
+	mov rax, 4
 	int 0x80 
-	popq rax 
-	popq rbx
-	popq rcx
-	popq rdx 
-	ret
+	pop rax 
+	pop rbx
+	pop rcx
+	pop rdx 
+	jmp _loopOrQuit 
 
 section .data 
 	msg db 'FizzBuzz', 0xa 
 	len  equ $ - msg
 	fizzMsg db 'Fizz', 0xa
-	fizzlen equ $ - msg
+	fizzlen equ $ - fizzMsg
 	buzzMsg db 'Buzz', 0xa 
-	buzzlen equ $ - msg 
+	buzzlen equ $ - buzzMsg
